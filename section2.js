a3d75c4efa5f9491b3bd384b64ead74d74c433c4
YeinSeo <74223296+YeinSeo@users.noreply.github.com>
2020-12-18 11:15:51 +0900

    섹션2 js 생성->html 연결

---
+++ section2.js
@@ -0,0 +1,21 @@
+// window.onload = function () {
+
+//     var a = document.getElementById("logo")
+//     document.getElementById('btn').addEventListener('click', function () {
+//         if (a.style.color == 'black') { a.style.color = 'red' }
+//         else if (a.style.color == 'red') { a.style.color = 'black' }
+//     });
+
+//     document.querySelector('.버튼2').addEventListener('click', function () {
+//         document.querySelector('.carousel').style.transform = 'translate(-100vw)';
+//     });
+
+//     document.querySelector('.버튼3').addEventListener('click', function () {
+//         document.querySelector('.carousel').style.transform = 'translate(-200vw)';
+//     });
+
+//     document.querySelector('.버튼1').addEventListener('click', function () {
+//         document.querySelector('.carousel').style.transform = 'translate(0vw)';
+//     });
+
+// }
